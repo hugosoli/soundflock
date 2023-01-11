beb6c38df72b9b07cb35be8afb3081a8bd59d6cb
Hugo <hugosoli@gmail.com>
2023-01-11 11:10:26 -0600

    Update servomotor.py

--- servomotor.py
+++ servomotor.py
@@ -1,19 +1,22 @@
 import time
 from adafruit_servokit import ServoKit
 
+
 pca = ServoKit(channels=16)
 pca.servo[0].set_pulse_width_range(500 , 2500)
+pca.servo[1].set_pulse_width_range(500 , 2500)
 pca.servo[0].actuation_range = 270
+pca.servo[1].actuation_range = 270
+
 for i in range(10):
-  pca.servo[0].angle = 0
-  time.sleep(4)
-  pca.servo[0].angle = 90
-  time.sleep(1)
-  pca.servo[0].angle = 0
+  pca.servo[0,1].angle = 0
+  time.sleep(2)
+  pca.servo[0,1].angle = 90
   time.sleep(2)
   pca.servo[0].angle = 180
-  time.sleep(1)
-  pca.servo[0].angle = 0
   time.sleep(2)
-  pca.servo[0].angle = 270
-  time.sleep(1)
+  pca.servo[1].angle = 0
+  time.sleep(2)
+  pca.servo[0,1].angle = 0
+  time.sleep(2)
+  
