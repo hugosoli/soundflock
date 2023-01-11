ec9e5c7d812a97e123c386a1013c03cbd788fabe
Hugo <hugosoli@gmail.com>
2023-01-11 10:29:30 -0600

    Update servomotor.py

--- servomotor.py
+++ servomotor.py
@@ -6,8 +6,14 @@ pca.servo[0].set_pulse_width_range(500 , 2500)
 pca.servo[0].actuation_range = 270
 for i in range(10):
   pca.servo[0].angle = 0
-  time.sleep(2)
+  time.sleep(4)
   pca.servo[0].angle = 90
+  time.sleep(1)
+  pca.servo[0].angle = 0
   time.sleep(2)
   pca.servo[0].angle = 180
-  time.sleep(4)
+  time.sleep(1)
+  pca.servo[0].angle = 0
+  time.sleep(2)
+  pca.servo[0].angle = 270
+  time.sleep(1)
